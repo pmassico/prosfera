7cea373cf470ba1480e41f90f0b2ba05dff7ec95
Phil Massicotte <pmass312@mtroyal.ca>
2019-10-30 20:22:04 -0600

    test variable

--- app/src/main/java/com/example/prosfera/MainActivity.java
+++ app/src/main/java/com/example/prosfera/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        String testString = "";
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
